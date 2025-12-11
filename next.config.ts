b0129dfbb4120039adab540443571f56d7c23263
Irsyad Jamal Pratama Putra <mr.icadj@gmail.com>
2025-12-11 08:23:46 +0000

    untuk logo tiento gunakan logo berikut: https://tiento.co.id/cdn/shop/fi

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tiento.co.id',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
