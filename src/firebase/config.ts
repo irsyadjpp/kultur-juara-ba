3fee9705e5e94ad23450ad33028d390b5c5ffe4d
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2026-01-28 10:03:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "kultur-juara-ba-59925955-1dbff",
+  "appId": "1:291819472204:web:4829f804ce1bc65aa5bbe9",
+  "apiKey": "AIzaSyAHBF71q0ndY7qBS9iU8WejyxLHvbBuMhA",
+  "authDomain": "kultur-juara-ba-59925955-1dbff.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "291819472204"
+};
