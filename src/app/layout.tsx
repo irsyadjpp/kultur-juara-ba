086754528d3544f681d506690876192e6d570f7d
Irsyad Jamal Pratama Putra <mr.icadj@gmail.com>
2026-02-01 14:02:26 +0700

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,18 @@ import { FirebaseClientProvider } from '@/firebase/client-provider';
 export const metadata: Metadata = {
   title: 'Kultur Juara PWN Indonesia Badminton Academy',
   description: 'Mencetak atlet bulutangkis masa depan dengan kurikulum profesional dan fasilitas modern di Kultur Juara PWN Indonesia Badminton Academy.',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon.ico' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicon/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png' },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
